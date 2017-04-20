ac68c998ebc9e8d312a30c7bf534f71a67171ea9
sparshturkane <rajaturkane99@gmail.com>
2017-04-20 17:06:07 +0530

    basic send an receive message functionality complete

--- index.js
+++ index.js
@@ -10,6 +10,31 @@ app.get('/', function(req, res) {
 io.on('connection', function(socket) {
     console.log('a user connected');
 
+    socket.on('INITIAL_SETUP', function(friendUserID, myUserID) {
+        var first;
+        var last;
+        var roomname;
+        var username = myUserID;
+        if(friendUserID > myUserID){
+            first = myUserID;
+            last = friendUserID;
+            roomname = first+last;
+        }else{
+            first = friendUserID;
+            last = myUserID;
+            roomname = first+last;
+        }
+        socket.room = roomname;
+        socket.join(roomname);
+        socket.username = username;
+        console.log(socket);
+        console.log(username +'---'+ roomname);
+    });
+
+    socket.on('SEND_MESSAGE_CLIENT', function(message) {
+        io.to(socket.room).emit('FORWARD_MESSAGE_SERVER', message, socket.username);
+    });
+
     socket.on('disconnect', function() {
         console.log('user disconnected');
     });
