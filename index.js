a98fb0ee7a67acb31952889ffae0d9be4ae3ebb9
sparshturkane <rajaturkane99@gmail.com>
2017-04-22 17:00:52 +0530

    done with basic polling, typing feature

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+// importing stuff
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mysql = require('mysql');
+var async = require("async");
+
+// Global variable
+var myUserIDG;
+var friendUserIDG;
+var myUserNameG;
+var friendUserNameG;
 
 // using db connection
 var dbConnection = mysql.createConnection({
@@ -20,22 +28,55 @@ var pool  = mysql.createPool({
     database: 'rockabyteServiceV3Test'
 });
 
+// pool callback functions
+// function queryExecutionMyUserID(err, connection) {
+//     var query = connection.query('SELECT * FROM userProfile WHERE userID = ?', [myUserIDG], function (error, results, fields) {
+//
+//         connection.release();// And done with the connection.
+//
+//         if (error) throw error; // Handle error after the release.
+//
+//         console.log(results[0].userName);
+//         myUserNameG = results[0].userName;
+//         callback();
+//     });
+// }
+//
+// function queryExecutionFriendUserID(err, connection) {
+//     var query = connection.query('SELECT * FROM userProfile WHERE userID = ?', [friendUserIDG], function (error, results, fields) {
+//
+//         connection.release();// And done with the connection.
+//
+//         if (error) throw error; // Handle error after the release.
+//
+//         console.log(results[0].userName);
+//         friendUserNameG = results[0].userName;
+//         callback();
+//     });
+// }
+
 // dbConnection.connect();
 app.get('/', function(req, res) {
     // res.send('<h1>Hello world</h1>');
     res.sendFile(__dirname + '/index.html');
 });
 
-var someVar = [];
+function userDetails(userName) {
+    this.userName = userName;
+}
 
 io.on('connection', function(socket) {
     console.log('a user connected');
 
     socket.on('INITIAL_SETUP', function(friendUserID, myUserID) {
+        myUserIDG = myUserID;
+        friendUserIDG = friendUserID;
+
         var first;
         var last;
         var roomname;
         var username = myUserID;
+
         if(friendUserID > myUserID){
             first = myUserID;
             last = friendUserID;
@@ -48,47 +89,83 @@ io.on('connection', function(socket) {
 
         var friendUserName ;
 
-        pool.getConnection(function(err, connection) {
-            // Use the connection
-            var query = connection.query('SELECT * FROM userProfile WHERE userID = ?', [myUserID], function (error, results, fields) {
-                // And done with the connection.
-                connection.release();
+        // async.parallel([
+        //     function(callback) { function1(callback); },
+        //     function(callback) { function2(callback); }
+        // ], function(err, values) {
+        //     function3(values[0], values[1]);
+        // });
 
-                // Handle error after the release.
-                if (error) throw error;
-                console.log(results);
-                // Don't use the connection here, it has been returned to the pool.
+        // async.parallel([
+        //     function(callback) { function1(callback); },
+        //     function(callback) { function2(callback); }
+        // ], function(err, values) {
+        //     function3(values[0], values[1]);
+        // });
 
-                socket.room = roomname; //we have this from both the userID's
-                socket.join(roomname); //we have this from both the userID's
-                socket.username = username; //myUserName
-                // here i will have to emmit details based on which the page will be setuped
-                // displayFriendName
-                // and older messages object which can be recovered later
+        async.parallel([
+            function(callback){
+                pool.getConnection(function queryExecutionMyUserID(err, connection) {
+                    var query = connection.query('SELECT * FROM userProfile WHERE userID = ?', [myUserIDG], function (error, results, fields) {
 
-                console.log(friendUserName);
-                console.log(someVar);
-                var initialDetailsObj = {
-                    friendUserName : results[0].userName,
-                    earlyMessages : [
-                        {
-                            msg:'hello'
-                        },
-                        {
-                            msg: 'reply asap',
-                        }
-                    ],
-                };
+                        connection.release();// And done with the connection.
 
-                console.log(initialDetailsObj);
-                io.to(socket.room).emit('INITIAL_DETAILS', initialDetailsObj);
-            });
-            console.log(query.results);
-        });
+                        if (error) throw error; // Handle error after the release.
+
+                        console.log(results[0].userName);
+                        myUserNameG = results[0].userName;
+                        callback();
+                    });
+                });
+            },function(callback){
+                pool.getConnection(function queryExecutionFriendUserID(err, connection) {
+                    var query = connection.query('SELECT * FROM userProfile WHERE userID = ?', [friendUserIDG], function (error, results, fields) {
+
+                        connection.release();// And done with the connection.
+
+                        if (error) throw error; // Handle error after the release.
+
+                        console.log(results[0].userName);
+                        friendUserNameG = results[0].userName;
+                        callback();
+                    });
+                });
+            }
+        ], function(err, results) {
+            socket.room = roomname; //we have this from both the userID's
+            socket.join(roomname); //we have this from both the userID's
+            socket.username = myUserNameG; //myUserName
+            // here i will have to emmit details based on which the page will be setuped
+            // displayFriendName
+            // and older messages object which can be recovered later
+            var initialDetailsObj = {
+                friendUserName : friendUserNameG,
+                currentUserName: myUserNameG,
+                earlyMessages : [
+                    {
+                        msg:'hello'
+                    },
+                    {
+                        msg: 'reply asap',
+                    }
+                ],
+            };
+
+            console.log(myUserNameG+" ======= "+friendUserNameG);
+            console.log(initialDetailsObj);
+            io.to(socket.id).emit('INITIAL_DETAILS', initialDetailsObj);
+        } );
+
+        // pool.getConnection(queryExecutionMyUserID);
     });
 
     socket.on('SEND_MESSAGE_CLIENT', function(message) {
         io.to(socket.room).emit('FORWARD_MESSAGE_SERVER', message, socket.username);
+    });
+
+    socket.on('FRIEND_TYPING',function() {
+        console.log('friend typing');
+        socket.broadcast.to(socket.room).emit('FRIEND_TYPING_CLIENT');
     });
 
     socket.on('disconnect', function() {
